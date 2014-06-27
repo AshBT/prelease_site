4c03b3668750d9704108483baf475b285d1b4778
Ashbt <ashley.beattie@gmail.com>
2014-06-27 12:34:29 -0400

    everything in, one more image to add

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,23 @@ RailsPrelaunchSignup::Application.routes.draw do
   resources :contact_form, only: [:new, :create, :index]
 
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'users#index'
+
   end
+
   devise_scope :user do
     root :to => "devise/registrations#new"
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    match '/user/sign_out' => 'sessions#destroy', :via => :delete, :as => :sign_out
   end
+
   devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations"}
   match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
+  
   resources :users do
     get 'invite', :on => :member
   end
-end
+
+  match 'sign_out' => 'sessions#destroy', :via => :delete, :as => :sign_out
+  # match "/blog" => "http://contextualyze.tumblr.com"
+  end
